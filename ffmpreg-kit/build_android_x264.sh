865bcee2499e2783ce6c280d8660b1f2e013d36f
Luis Finke <luisfinke@gmail.com>
2025-05-01 12:03:57 -0400

    bump java version, add android build script

---
+++ ffmpreg-kit/build_android_x264.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export ANDROID_SDK_ROOT=~/Library/Android/sdk
+export ANDROID_NDK_ROOT="$ANDROID_SDK_ROOT/ndk/22.1.7171670"
+./android.sh -d --enable-android-media-codec --enable-android-zlib --disable-arm-v7a --enable-x264 --enable-gpl || exit $?
