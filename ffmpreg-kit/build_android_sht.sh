41dc5246f1da9a8ed95143887a84f890bda3326c
jacobfenner <jacob@sevenhillstechnology.com>
2025-05-06 15:14:10 -0500

    remove debug flag, update ndk, update to v 7.1.1

--- ffmpreg-kit/build_android_sht.sh
+++ ffmpreg-kit/build_android_sht.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export ANDROID_SDK_ROOT=~/Library/Android/sdk
-export ANDROID_NDK_ROOT="$ANDROID_SDK_ROOT/ndk/22.1.7171670"
+export ANDROID_NDK_ROOT="$ANDROID_SDK_ROOT/ndk/29.0.13113456"
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk17/Contents/Home
-./android.sh -d --enable-android-media-codec --enable-android-zlib --disable-arm-v7a --enable-x264 --enable-gpl --speed || exit $?
+./android.sh --enable-android-media-codec --enable-android-zlib --disable-arm-v7a --enable-x264 --enable-gpl --speed || exit $?
